7854b66861823e672dfcc6c0edb33dca01925b58
maylaniksm <63834571+maylaniksm@users.noreply.github.com>
2020-09-14 10:51:07 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -80,80 +80,157 @@
 
 //SOAL 2
 // import library
+// import React, {Component} from 'react';
+// // class
+// class App extends Component {
+//     constructor(bank){
+//         super(bank);
+//         this.state = {
+//             nominal: '',
+//             bunga: '',
+//             periode: '12',
+//             hasilAngsuran: 'Angsuran per bulan:'
+//         };
+//         this.nominalChange = this.nominalChange.bind(this);
+//         this.bungaChange = this.bungaChange.bind(this);
+//         this.periodeChange = this.periodeChange.bind(this);
+//     }
+// nominalChange(event){
+//         this.setState({nominal: event.target.value})
+//     }
+// bungaChange(event){
+//         this.setState({bunga: event.target.value})
+//     }
+// periodeChange(event){
+//         this.setState({periode: event.target.value})
+//     }
+// menghitung = (event)  => {
+//         let nominal = this.state.nominal;
+//         let bunga = this.state.bunga;
+//         let periode = this.state.periode;
+
+//         // rumus
+//         let b = bunga / 100;
+//         let bungaa = (nominal * b) / periode; 
+//         let pokokk = nominal / periode;
+//         let angsurann = pokokk + bungaa;
+
+//         //output
+//         this.setState({hasilAngsuran: "Angsuran per bulan: Rp. " + angsurann})
+//         event.preventDefault();
+//     }
+// render(){
+//         return(
+//             <div className="card col-sm-5 mx-auto m-5">
+// 				<div className="card-header text-center bg-info text-white">
+// 					<h4>Cicilan Bank</h4>
+// 				</div>
+// 				<div className="card-body">
+//                     <div class="input-group mb-2">
+//                         <div class="input-group-prepend">
+//                         <label class="input-group-text" >Nominal : </label>
+//                         </div>
+//                         <input type="number" className="form-control" onChange={this.nominalChange}/>
+//                     </div>
+//                     <div class="input-group mb-2">
+//                         <div class="input-group-prepend">
+//                         <label class="input-group-text" >Bunga (%) : </label>
+//                         </div>
+//                         <input type="number" className="form-control" onChange={this.bungaChange}/>
+//                     </div>
+//                     <div class="input-group mb-2">
+//                         <div class="input-group-prepend">
+//                         <label class="input-group-text" >Periode : </label>
+//                         </div>
+//                         <input type="number" className="form-control" onChange={this.periodeChange}/>
+//                     </div>
+// 				</div>
+// 				<div className="card-footer">
+// 					<button className="form-control btn btn-warning text-white" onClick={this.menghitung}>
+// 						Hitung
+// 					</button>
+// 					<center><h5 className="text-center mt-4 btn-success">Hasil</h5></center>
+//                     <input className="form-control mb-1" value={this.state.hasilAngsuran} readOnly/>
+// 				</div>
+// 			</div>
+//         );
+//     }
+// }
+// export default App;
+
+//SOAL 3 PPN
+// import library
 import React, {Component} from 'react';
-// class
 class App extends Component {
-    constructor(bank){
-        super(bank);
-        this.state = {
-            nominal: '',
-            bunga: '',
-            periode: '12',
-            hasilAngsuran: 'Angsuran per bulan:'
-        };
-        this.nominalChange = this.nominalChange.bind(this);
-        this.bungaChange = this.bungaChange.bind(this);
-        this.periodeChange = this.periodeChange.bind(this);
+constructor(props){
+		super(props);
+		this.state = {
+			hargaawal: '',
+			ppn: '',
+			diskon: '',
+			hargaakhir: ''
+		};
+        this.hargaawalChange = this.hargaawalChange.bind(this);
+		this.ppnChange = this.ppnChange.bind(this);
+        this.diskonChange = this.diskonChange.bind(this);
+        this.menghitung = this.menghitung.bind(this);
+	}
+hargaawalChange(event) {
+		this.setState({hargaawal: event.target.value})
+	}
+ppnChange(event){
+		this.setState({ppn: event.target.value})
     }
-nominalChange(event){
-        this.setState({nominal: event.target.value})
+diskonChange(event){
+        this.setState({diskon: event.target.value})
     }
-bungaChange(event){
-        this.setState({bunga: event.target.value})
-    }
-periodeChange(event){
-        this.setState({periode: event.target.value})
-    }
-hitung = (event)  => {
-        let nominal = this.state.nominal;
-        let bunga = this.state.bunga;
-        let periode = this.state.periode;
+menghitung = (event) => {
+        const harga_awal = parseInt(this.state.hargaawal);
+        const ppn = parseInt(this.state.ppn) / 100;
+        const diskonan = parseInt(this.state.diskon) / 100;
+        const pajak = (ppn * harga_awal);
+        const disk = (harga_awal * diskonan);
+        const harga_akhir = harga_awal + pajak - disk;
 
-        // rumus
-        let b = bunga / 100;
-        let bungaa = (nominal * b) / periode; 
-        let pokokk = nominal / periode;
-        let angsurann = pokokk + bungaa;
-
-        //output
-        this.setState({hasilAngsuran: "Angsuran per bulan: Rp. " + angsurann})
-        event.preventDefault();
-    }
+        this.setState({hargaakhir: "Rp." + harga_akhir})
+			
+		event.preventDefault();
+	}
 render(){
-        return(
-            <div className="card col-sm-5 mx-auto m-5">
+		return(
+			<div className="card col-sm-5 mx-auto m-5">
 				<div className="card-header text-center bg-info text-white">
-					<h4>Cicilan Bank</h4>
+					<h4>Hitung Harga Akhir</h4>
 				</div>
 				<div className="card-body">
-                    <div class="input-group mb-2">
-                        <div class="input-group-prepend">
-                        <label class="input-group-text" >Nominal : </label>
-                        </div>
-                        <input type="number" className="form-control" onChange={this.nominalChange}/>
-                    </div>
-                    <div class="input-group mb-2">
-                        <div class="input-group-prepend">
-                        <label class="input-group-text" >Bunga (%) : </label>
-                        </div>
-                        <input type="number" className="form-control" onChange={this.bungaChange}/>
-                    </div>
-                    <div class="input-group mb-2">
-                        <div class="input-group-prepend">
-                        <label class="input-group-text" >Periode : </label>
-                        </div>
-                        <input type="number" className="form-control" onChange={this.periodeChange}/>
-                    </div>
+					<div class="input-group mb-2">
+						<div class="input-group-prepend">
+						<label class="input-group-text" >Harga Awal : </label>
+						</div>
+						<input type="number" className="form-control" onChange={this.hargaawalChange}/>
+					</div>
+					<div class="input-group mb-2">
+						<div class="input-group-prepend">
+						<label class="input-group-text" >PPN (%) : </label>
+						</div>
+						<input type="number" className="form-control" onChange={this.ppnChange}/>
+					</div>
+					<div class="input-group mb-2">
+						<div class="input-group-prepend">
+						<label class="input-group-text" >Diskon (%) : </label>
+						</div>
+						<input type="number" className="form-control" onChange={this.diskonChange}/>
+					</div>
 				</div>
 				<div className="card-footer">
-					<button className="form-control btn btn-warning text-white" onClick={this.hitung}>
+					<button className="form-control btn btn-danger" onClick={this.menghitung} >
 						Hitung
 					</button>
-					<center><h5 className="text-center mt-4 btn-success">Hasil</h5></center>
-                    <input className="form-control mb-1" value={this.state.hasilAngsuran} readOnly/>
+					<h4 className="text-center mt-4 bg-warning">Harga Akhir</h4>
+					<input className="form-control" value={this.state.hargaakhir} readOnly/>
 				</div>
 			</div>
-        );
-    }
+		);
+	}
 }
 export default App;
